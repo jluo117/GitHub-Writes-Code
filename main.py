b54e3e3852e0ed308e1694d603120f95bc9d8f6a
james luo <luojames52@gmail.com>
2018-10-24 18:47:46 -0700

    add fizzbuzz

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def fizz_buzz():
+    for i in range(1,100):
+        if i % 15 == 0:
+            print("fizzbuzz")
+        if i % 3 == 0:
+            print("fizz")
+        if i % 5 == 0:
+            print("buzz")
 def main():
     print("Let's get started!")
+    fizz_buzz()
 
 if __name__ == "__main__":
     main()
